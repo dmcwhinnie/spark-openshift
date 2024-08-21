e26cae657b030907b88ad32f278c2f37ac38fc2f
David McWhinnie <dmcwhinnie@users.noreply.github.com>
2024-08-21 12:52:14 -0500

    Update common.sh

--- Docker Image/common.sh
+++ Docker Image/common.sh
@@ -2,3 +2,9 @@
 
 # unset variable set by kubernetes
 unset SPARK_MASTER_PORT
+
+mkdir -p /tmp/.java/.systemPrefs
+mkdir /tmp/.java/.userPrefs
+chmod -R 755 /tmp/.java
+export JAVA_OPTS="-Djava.util.prefs.systemRoot=/tmp/.java -Djava.util.prefs.userRoot=/tmp/.java/.userPrefs"
+  
